ab48fef0e76845bcc3225341728c564367b34ded
Angel M De Miguel <angel@bitnami.com>
2017-02-09 18:12:07 +0100

    Fix id_rsa location

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,10 +12,10 @@ DEPLOY_URL=bitnami-ui-dev.nami
 
 ## Configure ssh key for deployment
 if  [ -f $SSH_KEY ]; then
-  mkdir -p /home/root/.ssh
-  cp $SSH_KEY /home/root/.ssh/id_rsa
-  chown root:root /home/root/.ssh/id_rsa
-  chmod 600 /home/root/.ssh/id_rsa
+  mkdir -p /root/.ssh
+  cp $SSH_KEY /root/.ssh/id_rsa
+  chown root:root /root/.ssh/id_rsa
+  chmod 600 /root/.ssh/id_rsa
 fi
 
 # Compile the project. Now, we are deploying to staging server, so
@@ -24,5 +24,5 @@ npm run docs:build
 
 # Copy the generated output to the server
 if [ $? -eq 0 ]; then
-  scp -v -r -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null $DEPLOY_SRC/* $DEPLOY_USER@$DEPLOY_URL:$DEPLOY_DEST
+  scp -r -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null $DEPLOY_SRC/* $DEPLOY_USER@$DEPLOY_URL:$DEPLOY_DEST
 fi
