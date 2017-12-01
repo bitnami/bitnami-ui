8d1e611f503872fa279fd8013cef62864864c963
Angel M De Miguel <angel@bitnami.com>
2017-12-01 10:57:33 +0100

    Fix the publish script. Don't publish if there isn't any new tag

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,5 +2,11 @@
 set -e
 
 # Install and publish
-npm install
-npm run publish
+if git describe --exact-match >&/dev/null; then
+  TAG=$(git describe --exact-match)
+  echo "Deploying $TAG tag"
+  npm install
+  npm run publish
+else
+  echo "No new tags. Skipping the publishing"
+fi
