c3eba61fabddbf0862e768e4df8fb4b455f13a0b
Angel M De Miguel <angel@bitnami.com>
2018-03-15 14:06:27 +0100

    Revert "Allow publish script to get a tag to deploy"
    This reverts commit e77bb456968917c9d2cae7f38ad2b28c4caa4141.

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 set -e
 
-publish() {
-  echo "Deploying $1 tag"
-
-  # Check if it's required
-  CURRENT_TAG=$(git describe --exact-match)
-
-  if [ "$1" -ne "$CURRENT_TAG" ]; then
-    git checkout $1
-  fi
-
-  # Install
-  yarn install
+# Install and publish
+if git describe --exact-match >&/dev/null; then
+  TAG=$(git describe --exact-match)
+  echo "Deploying $TAG tag"
 
   # Build
   lerna bootstrap
@@ -22,14 +14,6 @@ publish() {
 
   # Prepare the folder
   BUCKET=$BUCKET yarn run cdn
-}
-
-# Install and publish
-if git describe --exact-match >&/dev/null; then
-  TAG=$(git describe --exact-match)
-  publish $TAG
-elif [[ -v DEPLOY_TAG ]]; then
-  publish $DEPLOY_TAG
 else
   echo "No new tags. Skipping the publishing"
 fi
