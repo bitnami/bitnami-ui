8d4f58f41baf4143b18ed52dd945cc1583f76a2e
Angel M De Miguel <angel@bitnami.com>
2018-09-11 12:21:08 +0200

    Add the build name and project to the integration tests

--- packages/hex-react/setupIntegrationTests.js
+++ packages/hex-react/setupIntegrationTests.js
@@ -5,13 +5,15 @@ import capabilities from './capabilities';
 // Set a bigger timeout
 jest.setTimeout(60000);
 
-// let bsLocal;
+const buildName = process.env.TRAVIS_COMMIT || 'local';
 
 // Add the capabilities
 const capability = process.env.CAPABILITY || Object.keys(capabilities)[0];
 config.desiredCapabilities = Object.assign(capabilities[capability], {
   'browserstack.local': true,
   'browserstack.debug': true,
+  build: buildName,
+  project: 'hex-react',
 });
 
 // Global callbacks
