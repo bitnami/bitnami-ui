d1ae0563db941272f7ce22e29b0520cd8e43cf0c
Angel M De Miguel <angel@bitnami.com>
2019-05-03 16:22:16 +0200

    Fix the jest integration environment after upgrading to jest 24

--- packages/hex-react/__integration__/render.js
+++ packages/hex-react/__integration__/render.js
@@ -2,7 +2,8 @@ describe('Renders correctly', () => {
   it('renders on different browsers', async () => {
     await browser.url('http://127.0.0.1:9000');
     await browser.pause(1000);
-    const text = await browser.getText('#title');
+    const title = await browser.$('#title');
+    const text = await title.getText();
 
     expect(text).toBe('HEx');
   });
