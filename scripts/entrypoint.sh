ef84cee312caa93da168e7857b7e18d519672f9f
Angel M De Miguel <angel@bitnami.com>
2019-05-06 10:51:32 +0200

    Build main packages before the hex-docs

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -9,5 +9,10 @@ yarn
 echo "Bootstraping the project with lerna"
 lerna bootstrap --npm-client=yarn
 
+# Build main projects. We need to do it because we're using root user on the docker image
+# It causes the prepublish script to fail due to https://docs.npmjs.com/misc/scripts#user
+lerna run build --scope @bitnami/hex-core
+lerna run build --scope @bitnami/hex
+
 # Now, we just launch the command
 exec "$@"
